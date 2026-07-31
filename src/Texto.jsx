c8884b7a80a089d7f26d3ecfde8b1aa1d7ae1403
christiang <christiangmc72@gmail.com>
2026-07-30 20:20:17 -0600

    texto

---
+++ src/Texto.jsx
@@ -0,0 +1,46 @@
+function Texto() {
+  return (
+    <div>
+      <h1>La Ilíada - Canto I: La Cólera de Aquiles</h1>
+      
+      <p>
+        Es un sistema que almacena información en una base de datos y al hacer cambios se genera una alerta al administrador en tiempo real.
+        permite el monitoreo en tiempo real, la detección anticipada de fallas y la optimización de recursos
+        Beneficios Principales
+        Datos al instante: Conoces el estado de máquinas o procesos en tiempo real.
+        Menos gastos: Ayuda a prevenir fallas lo cual ayuda a ahorrar dinero en reparaciones. 
+        Aviso de problemas: Manda alertas antes de que una pieza o sistema se rompa por completo.
+      </p>
+
+      <p>
+        Este proyecto se puede aplicar en varias áreas de trabajo que se generen datos para recopilar y almacenar en una base de datos
+        En algunos ámbitos que se puede emplear son los siguientes :
+        Transporte y Flotillas: Control de ubicación GPS, nivel de combustible, velocidad y estado del motor en camiones de carga o reparto.
+        Manufactura y Fábricas: Supervisión de temperatura, vibración y desgaste en máquinas grandes para hacer mantenimiento antes de que fallen.
+        Sector Salud: Seguimiento remoto de signos vitales en pacientes o control de temperatura en refrigeradores de medicinas y vacunas.
+        Agricultura: Medición de humedad en la tierra, clima y control de sistemas de riego a distancia.
+        Energía y Minería: Vigilancia de pozos petroleros, consumo de luz/agua y control de vehículos o maquinaria pesada en zonas alejadas
+      </p>
+
+      <p>
+        Es un sistema creado para la telemetria de los dispositivos para asi monitorearlos
+        de una manera mas precisa en caso de que alguno de ellos tenga una anomalia generar alertas
+      </p>
+
+      <p>
+        Es un sistema creado para la telemetria de los dispositivos 
+      </p>
+
+      <p>
+        —Que yo no te encuentre, anciano, cerca de las cóncavas naves, ya sea
+        demorándote ahora, ya volviendo más tarde; pues quizás no te valgan el
+        cetro y las ínfulas del dios. A aquella no la soltaré; antes le llegará la
+        vejez en mi casa, en Argos, lejos de su patria, devanando la telaraña y
+        compartiendo mi lecho. Pero vete; no me irrites, para que salgas más sano
+        y salvo.
+      </p>
+    </div>
+  );
+}
+
+export default Texto;
